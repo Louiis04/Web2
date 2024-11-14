f3f00cc59eb449a4472daa6003c8cfaae824aa23
Louiis04 <luiseduardosport1000@gmail.com>
2024-11-13 21:15:31 -0300

    Atividade-03 + Captura-De-Tela

--- atividade-web3/database/seeders/DatabaseSeeder.php
+++ atividade-web3/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             AuthorPublisherBookSeeder::class,
+            UserBorrowingSeeder::class,
         ]);
     }
 }
